65589ab7fa3dc6f6759afa980dbb042094b1aebe
AlexiLuises <alexi.luises18@bathspa.ac.uk>
2022-03-26 21:13:32 +0000

    start of project

---
+++ components/Loader.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Loader = ({ show }) => {
+  return show ? <div className="loader"></div> : null;
+};
+export default Loader;
---
+++ pages/admin/[slug].js
@@ -0,0 +1,9 @@
+export default function AdminPostEdit({}) {
+  return (
+    <main>
+      <h1>edit?</h1>
+      <h2>maybe</h2>
+      <h3>maybe not</h3>
+    </main>
+  );
+}
